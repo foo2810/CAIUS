278d8b88fa09d4e3cd6326c64af71fedfb82359a
Satoshi <watchosastrom3104>
2020-06-18 16:32:02 +0900

    a little update

--- sample_keras.py
+++ sample_keras.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.path.append("./")
 
 import numpy as np
@@ -6,7 +7,7 @@ import tensorflow as tf
 
 from utils.datasets import load_data, train_test_split
 from utils.common import time_counter
-from models.wrapper import VGG16
+from models.wrapper import VGG16, VGG19
 
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.utils import to_categorical
@@ -14,13 +15,14 @@ from tensorflow.keras.utils import to_categorical
 # Params
 batch_size = 64
 n_epochs = 20
-lr = 0.001
+lr = 1e-4
+img_size = 128
 
 # Dataset
 print('[Dataset]')
 with time_counter():
-    x, y, _ = load_data('data/dataset/', classes=['normal', 'nude', 'swimwear'], size=256,
-                        cache_path='data/data_cache/dataset_size256.pkl')
+    x, y, _ = load_data('data/dataset/', classes=['normal', 'nude', 'swimwear'], size=img_size,
+                        cache_path='data/data_cache/dataset_size' + str(img_size) + '.pkl')
     x_train, x_test, y_train, y_test = train_test_split(x, y, train_rate=0.5)
     n_train = len(x_train)
     # train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(n_train).batch(batch_size)
@@ -35,10 +37,15 @@ y_train = to_categorical(y_train, num_classes=3)
 y_test = to_categorical(y_test, num_classes=3)
 
 # Model
-model = VGG16(weights=None, classes=3, input_shape=(256, 256, 3))
+model = VGG16(weights=None, classes=3, input_shape=(img_size, img_size, 3))
 
 # Training
-model.compile(optimizer=Adam(lr), loss="categorical_crossentropy", metrics=["Accuracy"])
+model.compile(optimizer=Adam(lr), loss='categorical_crossentropy',
+              metrics=['accuracy'])
 
 stack = model.fit(x=x_train, y=y_train, batch_size=batch_size, epochs=n_epochs,
                   validation_data=(x_test, y_test), verbose=1)
+
+score = model.evaluate(x_test, y_test)
+print('Val Accuracy: {}'.format(score[1]))
+print('Val Loss: {}'.format(score[0]))
