88fecc8d7111fd529b901e3943bfcb3c27084a99
daikon620 <40687123+daikon620@users.noreply.github.com>
2020-06-21 18:24:31 +0900

    refactor supCon.py

--- sample_supCon.py
+++ sample_supCon.py
@@ -57,78 +57,80 @@ if not result_path.exists():
 # https://github.com/sayakpaul/Supervised-Contrastive-Learning-in-TensorFlow-2 からのコピペ
 # =============================================================================
 
-# =============================================================================
-# Encoder nework and Projection network 自己教師ありの部分
-# =============================================================================
-class UnitNormLayer(tf.keras.layers.Layer):
-    # Reference: https://github.com/wangz10/contrastive_loss/blob/master/model.py
-    '''Normalize vectors (euclidean norm) in batch to unit hypersphere.
-    '''
-    def __init__(self):
-        super().__init__()
+def training_SupCon_Encoder(encoder_model=None, train_ds=None, optimizer=None, n_epochs=20):
+    """
+    Encoder nework and Projection network 自己教師ありの部分
+    """
 
-    def call(self, input_tensor):
-        norm = tf.norm(input_tensor, axis=1)
-        return input_tensor / tf.reshape(norm, [-1, 1])
-    
-# Encoder Network
-def encoder_net():
-	inputs = tfk.Input((128, 128, 3))
-	normalization_layer = UnitNormLayer()
+    if train_ds is None:
+        raise ValueError("train_ds most not be None")
 
-	encoder = ResNet50(weights=None, include_top=False)
-	encoder.trainable = True
+    if encoder_model is None:
+        encoder_model = ResNet50(weights=None, include_top=False)
 
-	embeddings = encoder(inputs, training=True)
-	embeddings = tfk.layers.GlobalAveragePooling2D()(embeddings)
-	norm_embeddings = normalization_layer(embeddings)
-
-	encoder_network = tfk.Model(inputs, norm_embeddings)
-
-	return encoder_network
-
-# Projector Network
-def projector_net():
-	projector = tfk.models.Sequential([
-		tfk.layers.Dense(256, activation="relu"),
-		UnitNormLayer()
-	])
-
-	return projector
-
-# Training the encoder and the projector
-
-# SGD with lr decay function
-optimizer = tfk.optimizers.Adam(learning_rate=1e-3)
-
-encoder_r = encoder_net()
-projector_z = projector_net()
-
-supConloss = SupConLoss()
-
-@tf.function
-def train_step(images, labels):
-	with tf.GradientTape() as tape:
-		r = encoder_r(images, training=True)
-		z = projector_z(r, training=True)
-		loss = supConloss(z, labels)
-
-	gradients = tape.gradient(loss, 
-		encoder_r.trainable_variables + projector_z.trainable_variables)
-	optimizer.apply_gradients(zip(gradients, 
-		encoder_r.trainable_variables + projector_z.trainable_variables))
-
-	return loss
-
-EPOCHS = 20
-LOG_EVERY = 1
-train_loss_results = []
-
-print('[training representations]')
-with time_counter():
-    for epoch in range(EPOCHS):	
-        epoch_loss_avg = tf.keras.metrics.Mean()
+    if optimizer is None:
+        optimizer = tfk.optimizers.Adam(learning_rate=1e-3)
         
+
+    class UnitNormLayer(tf.keras.layers.Layer):
+        # Reference: https://github.com/wangz10/contrastive_loss/blob/master/model.py
+        '''Normalize vectors (euclidean norm) in batch to unit hypersphere.
+        '''
+        def __init__(self):
+            super().__init__()
+
+        def call(self, input_tensor):
+            norm = tf.norm(input_tensor, axis=1)
+            return input_tensor / tf.reshape(norm, [-1, 1])
+        
+    # Encoder Network
+    def encoder_net(encoder_model=ResNet50(weights=None, include_top=False)):
+        inputs = tfk.Input((128, 128, 3))
+        normalization_layer = UnitNormLayer()
+
+        encoder = encoder_model
+        encoder.trainable = True
+
+        embeddings = encoder(inputs, training=True)
+        embeddings = tfk.layers.GlobalAveragePooling2D()(embeddings)
+        norm_embeddings = normalization_layer(embeddings)
+
+        encoder_network = tfk.Model(inputs, norm_embeddings)
+
+        return encoder_network
+
+    # Projector Network
+    def projector_net(encoder_r: tfk.Model):
+        encoder_r.trainable = True
+        projector = tfk.models.Sequential([
+            encoder_r,
+            tfk.layers.Dense(256, activation="relu"),
+            UnitNormLayer()
+        ])
+
+        return projector
+
+    # Training the encoder and the projector
+
+    encoder_r = encoder_net(encoder_model)
+    projector_z = projector_net(encoder_r)
+    supConloss = SupConLoss()
+
+    @tf.function
+    def train_step(images, labels):
+        with tf.GradientTape() as tape:
+            z = projector_z(images, training=True)
+            loss = supConloss(z, labels)
+
+        gradients = tape.gradient(loss, projector_z.trainable_variables)
+        optimizer.apply_gradients(zip(gradients, projector_z.trainable_variables))
+
+        return loss
+
+    train_loss_results = []
+
+    epoch_loss_avg = tf.keras.metrics.Mean()
+    for epoch in range(n_epochs):	
         for (images, labels) in train_ds:
             loss = train_step(images, labels)
             epoch_loss_avg.update_state(loss) 
@@ -137,18 +139,25 @@ with time_counter():
             if tf.math.is_nan(loss):
                 print("loss", loss)
                 print("encoder_r output is nan:", tf.reduce_any(tf.math.is_nan(encoder_r(images))))
-                print("projector_z output is nan:", tf.reduce_any(tf.math.is_nan(projector_z(encoder_r(images)))))
+                print("projector_z output is nan:", tf.reduce_any(tf.math.is_nan(projector_z(images))))
                 sys.exit()
 
-        train_loss_results.append(epoch_loss_avg.result())
-        # print({"supervised_contrastive_loss": epoch_loss_avg.result()})
+        print("Epoch[{}/{}] Loss: {:.3f}".format(epoch+1, n_epochs, epoch_loss_avg.result()))
 
-        if epoch % LOG_EVERY == 0:
-            print("Epoch: {} Loss: {:.3f}".format(epoch, epoch_loss_avg.result()))
+        train_loss_results.append(epoch_loss_avg.result().numpy())
+
+        epoch_loss_avg.reset_states()
+
+    hist = {"'supervised_contrastive_loss'": train_loss_results}
+    return encoder_r, hist
+
+# SGD with lr decay function
+optimizer = tfk.optimizers.Adam(learning_rate=1e-3)
+
+encoder_r, hist = training_SupCon_Encoder(train_ds=train_ds, optimizer=optimizer, n_epochs=15)
 
 supCon_file_path = str(result_path / 'supCon.csv')
-pd.DataFrame(train_loss_results, columns=['supervised_contrastive_loss']).to_csv(supCon_file_path)
-
+pd.DataFrame(hist).to_csv(supCon_file_path)
 
 # =============================================================================
 # Classifer 部分　教師あり学習
@@ -156,7 +165,7 @@ pd.DataFrame(train_loss_results, columns=['supervised_contrastive_loss']).to_csv
 
 # model
 num_classes = 3 # TODO Output size
-def supervised_model():
+def supervised_model(encoder_r: tfk.Model):
 	inputs = tfk.Input((128, 128, 3))
 	encoder_r.trainable = False
 
@@ -167,7 +176,7 @@ def supervised_model():
   
 	return supervised_model
 
-model = supervised_model()
+model = supervised_model(encoder_r)
 
 # Loss
 loss = tfk.losses.SparseCategoricalCrossentropy()
